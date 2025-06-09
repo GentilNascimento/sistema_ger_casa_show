b9fa38892e4ed29f72fe762c4a98e1bde3b5f0cb
Gentil-dev <gentiln65@gmail.com>
2025-06-09 12:00:09 -0300

    Update wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -7,6 +7,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 application = get_wsgi_application()
 
 #Força a inicialização do scheduler apenas na Railway
-if os.environ.get('RAILWAY_ENVIRONMENT_NAME') == 'production' or os.environ.get('RENDER_EXTERNAL_HOSTNAME':  
+if os.environ.get('RAILWAY_ENVIRONMENT_NAME') == 'production' or os.environ.get('RENDER_EXTERNAL_HOSTNAME'):  
     from app.scheduler import start_scheduler
     start_scheduler()
